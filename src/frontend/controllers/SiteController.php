f52bbdaa794365b5a8e41e7316f87453e2913fe2
Brian McGee <brian@mobilejazz.com>
2017-08-31 11:24:30 +0200

    Added a missing import

--- src/frontend/controllers/SiteController.php
+++ src/frontend/controllers/SiteController.php
@@ -15,6 +15,7 @@ use yii;
 use yii\base\InvalidParamException;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
+use yii\helpers\ArrayHelper;
 use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\web\ForbiddenHttpException;
